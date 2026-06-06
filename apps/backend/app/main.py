3b48fc9633cf09640449b905e53deb96d7be32a2
degiwo <d.wong93@hotmail.de>
2026-06-06 19:40:01 +0200

    add get enpoint for results

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from app.results import store_result
+from app.results import store_result, get_results
 
 app = FastAPI()
 
@@ -9,6 +9,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "Worlds"}
+
+
+@app.get("/results")
+def read_results():
+    return get_results()
 
 
 @app.post("/results")
--- apps/backend/app/results.py
+++ apps/backend/app/results.py
@@ -1,6 +1,11 @@
 import json
 
 
+def get_results():
+    with open("results.json", "r") as f:
+        return json.load(f)
+
+
 def store_result(result: dict):
     with open("results.json", "w") as f:
         json.dump(result, f)
