b50fe4cbe3763bf7ab48de944753cdfc60003813
degiwo <d.wong93@hotmail.de>
2026-06-16 10:49:13 +0200

    add first frontend implementation

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,12 +1,27 @@
+import { provideHttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class App {
   protected readonly title = signal('Snooker App');
+
+  backend_url = 'http://localhost:8000';
+  client = provideHttpClient();
+
+  form = new FormGroup({
+    opponent: new FormControl(''),
+    player_score: new FormControl(0),
+    opponent_score: new FormControl(0),
+  });
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
